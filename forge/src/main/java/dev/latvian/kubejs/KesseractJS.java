2206798051abc4f0068c9174baa894741c7ea373
ZZZank <3410764033@qq.com>
2024-10-06 16:34:22 +0800

    register KesseractJS with modid `kesseractjs`
    for better dependency management in the future

---
+++ forge/src/main/java/dev/latvian/kubejs/KesseractJS.java
@@ -0,0 +1,12 @@
+package dev.latvian.kubejs;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author ZZZank
+ */
+@Mod(KesseractJS.MOD_ID)
+public class KesseractJS {
+    public static final String MOD_ID = "kesseractjs";
+    public static final String NAME = "KesseractJS";
+}
