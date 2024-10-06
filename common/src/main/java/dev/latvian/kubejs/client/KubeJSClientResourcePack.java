28aba5794d3067ead89f4573c726a0e8964f6646
ZZZank <3410764033@qq.com>
2024-10-06 16:24:56 +0800

    disable lang file reading for now

--- common/src/main/java/dev/latvian/kubejs/client/KubeJSClientResourcePack.java
+++ common/src/main/java/dev/latvian/kubejs/client/KubeJSClientResourcePack.java
@@ -3,7 +3,6 @@ package dev.latvian.kubejs.client;
 import com.google.gson.JsonElement;
 import dev.latvian.kubejs.KubeJS;
 import dev.latvian.kubejs.KubeJSEvents;
-import dev.latvian.kubejs.KubeJSPaths;
 import dev.latvian.kubejs.client.asset.GenerateClientAssetsEventJS;
 import dev.latvian.kubejs.client.asset.LangEventJS;
 import dev.latvian.kubejs.generator.AssetJsonGenerator;
@@ -11,25 +10,23 @@ import dev.latvian.kubejs.registry.RegistryInfos;
 import dev.latvian.kubejs.script.ScriptType;
 import dev.latvian.kubejs.script.data.KubeJSResourcePack;
 import dev.latvian.kubejs.util.KubeJSPlugins;
-import dev.latvian.mods.rhino.mod.util.JsonUtils;
+import lombok.val;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class KubeJSClientResourcePack extends KubeJSResourcePack {
 	public static List<PackResources> inject(List<PackResources> packs) {
 		if (KubeJS.instance == null) {
 			return packs;
 		}
-		List<PackResources> injected = new ArrayList<>(packs);
+		val injected = new ArrayList<>(packs);
 		//KubeJS injected resources should have lower priority then user resources packs, which means file pack
 		int pos = injected.size();
 		for (int i = 0; i < pos; i++) {
@@ -55,33 +52,34 @@ public class KubeJSClientResourcePack extends KubeJSResourcePack {
 	}
 
 	private void generateLang(AssetJsonGenerator generator) {
-		var langEvent = new LangEventJS();
+		val langEvent = new LangEventJS();
 
-		for (var builder : RegistryInfos.ALL_BUILDERS) {
+		for (val builder : RegistryInfos.ALL_BUILDERS) {
 			builder.generateLang(langEvent);
 		}
 
-		Map<String, String> langMap = new HashMap<>();
+		val langMap = new HashMap<String, String>();
 		KubeJSPlugins.forEachPlugin(p -> p.generateLang(langMap));
 
         new GenerateClientAssetsEventJS(generator).post(ScriptType.CLIENT, KubeJSEvents.CLIENT_GENERATE_ASSET);
 
+        /*
 		//read lang json and add into lang event
-		try (var in = Files.list(KubeJSPaths.ASSETS)) {
-			for (var dir : in.filter(Files::isDirectory).collect(Collectors.toList())) {
-				var langDir = dir.resolve("lang");
+		try (val in = Files.list(KubeJSPaths.ASSETS)) {
+			for (val dir : in.filter(Files::isDirectory).collect(Collectors.toList())) {
+				val langDir = dir.resolve("lang");
 				if (!Files.exists(langDir) || !Files.isDirectory(langDir)) {
 					continue;
 				}
-				var namespace = dir.getFileName().toString();
-				for (var path : Files.list(langDir).filter(Files::isRegularFile).filter(Files::isReadable).collect(Collectors.toList())) {
-					var fileName = path.getFileName().toString();
+				val namespace = dir.getFileName().toString();
+				for (val path : Files.list(langDir).filter(Files::isRegularFile).filter(Files::isReadable).collect(Collectors.toList())) {
+					val fileName = path.getFileName().toString();
 					if (!fileName.endsWith(".json")) {
 						continue;
 					}
-					try (var reader = Files.newBufferedReader(path)) {
-						var json = JsonUtils.GSON.fromJson(reader, Map.class);
-						var lang = fileName.substring(0, fileName.length() - 5);
+					try (val reader = Files.newBufferedReader(path)) {
+						val json = JsonUtils.GSON.fromJson(reader, Map.class);
+						val lang = fileName.substring(0, fileName.length() - 5);
 						langEvent.get(namespace, lang).addAll(json);
 					} catch (Exception ex) {
 						ex.printStackTrace();
@@ -91,13 +89,14 @@ public class KubeJSClientResourcePack extends KubeJSResourcePack {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+         */
 
 		//using a special namespace to keep backward equivalence
 		langEvent.get("kubejs_generated", "en_us").addAll(langMap);
         langEvent.post(ScriptType.CLIENT, KubeJSEvents.CLIENT_LANG);
 
-		for (var lang2entries : langEvent.namespace2lang2entries.values()) {
-			for (var entries : lang2entries.values()) {
+		for (val lang2entries : langEvent.namespace2lang2entries.values()) {
+			for (val entries : lang2entries.values()) {
 				generator.json(entries.path(), entries.asJson());
 			}
 		}
