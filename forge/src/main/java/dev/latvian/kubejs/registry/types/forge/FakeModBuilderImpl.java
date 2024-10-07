5757325b37dce337f36485201e951828c8299b1a
Gcat101 <gcatmail2@gmail.com>
2024-10-07 21:48:32 +0300

    Revert "use mixin for ModList accessing" until zzzank fixes his shit

--- forge/src/main/java/dev/latvian/kubejs/registry/types/forge/FakeModBuilderImpl.java
+++ forge/src/main/java/dev/latvian/kubejs/registry/types/forge/FakeModBuilderImpl.java
@@ -1,20 +1,18 @@
 package dev.latvian.kubejs.registry.types.forge;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 import dev.latvian.kubejs.forge.FakeModInfo;
-import dev.latvian.kubejs.mixin.forge.AccessModList;
 import dev.latvian.kubejs.registry.types.FakeModBuilder;
-import lombok.val;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
 
 /**
  * @author G_cat101
  */
-public abstract class FakeModBuilderImpl extends FakeModBuilder {
-    public FakeModBuilderImpl() {
-        super(null);
-    }
-
+@SuppressWarnings("unchecked")
+public class FakeModBuilderImpl {
     public static class FakeModContainer extends ModContainer {
         public FakeModContainer(FakeModInfo info) {
             super(info);
@@ -24,23 +22,33 @@ public abstract class FakeModBuilderImpl extends FakeModBuilder {
         public boolean matches(Object mod) {
             return false;
         }
-
         @Override
         public Object getMod() {
             return null;
         }
     }
+    
+    private static Map<String, ModContainer> indexedMods;
+    static {
+        ModList modList = ModList.get();
+
+        try {
+            Field indexedModsField = modList.getClass().getDeclaredField("indexedMods");
+            indexedModsField.setAccessible(true);
+            indexedMods = (Map<String, ModContainer>)indexedModsField.get(modList);
+        } catch (Exception ex) {
+			ex.printStackTrace();
+		}
+    }
 
     public static void addFakeMod(FakeModBuilder builder) throws Exception {
-        val access = ((AccessModList) ModList.get());
-        if (access.indexedMods().containsKey(builder.modId)) {
+        if (indexedMods.containsKey(builder.modId)) {
             throw new IllegalArgumentException("Tried to create a fake mod with id '" + builder.modId + "', but a mod with that id already exists.");
         }
 
-        val info = new FakeModInfo(builder);
-        val container = new FakeModContainer(info);
+        FakeModInfo info = new FakeModInfo(builder);
+        FakeModContainer container = new FakeModContainer(info);
 
-        access.mods().add(container);
-        access.indexedMods().put(builder.modId, container);
+        indexedMods.put(builder.modId, container);
     }
 }
